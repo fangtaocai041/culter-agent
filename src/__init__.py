65feb0737f2a1e62e1c364f44f2be45154eed3af
fangtaocai041 <fangtaocai041@gmail.com>
2026-06-22 12:34:04 +0800

    更新: src模块 + README

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+
+import sys as _sys
+from pathlib import Path as _Path
+_PROJECT_ROOT = str(_Path(__file__).resolve().parent.parent)
+if _PROJECT_ROOT not in _sys.path:
+    _sys.path.insert(0, _PROJECT_ROOT)
